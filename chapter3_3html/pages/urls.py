ec48b45650f5a5597eb469014b226dad8bde7850
huyreeve <huyreeve@email.com>
2022-11-01 10:50:48 +0700

    ok

---
+++ chapter3_3html/pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from .views import homePageView
+from .views import HomePageView, AboutPageView  # newww
+
+# urlpatterns = [
+#     path("", homePageView, name="home"),
+# ]
+
+urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),  # new
+    path("", HomePageView.as_view(), name="home"),
+]
